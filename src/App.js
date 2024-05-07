f1f02b0d8abbf679ed00500c6677b9bf9615d0f7
Greg <me@greg-mitten.dev>
2024-05-07 22:03:47 +0100

    Added card rarity and cards.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import ChatPanel from "./components/ChatPanel";
-import CenterPanel from "./components/CenterPanel";
-import LeftPanel from "./components/LeftPanel";
-import RightPanel from "./RightPanel";
-import Card from "./components/Card";
+import Card, {CARD_QUALITY, CARD_TYPE} from "./components/Card";
 
 function range(start = 0, end) {
     return Array.from({length: end - start}, (v, k) => k + start);
@@ -15,9 +11,17 @@ function App() {
             <div className={"background-container"}>
                 <div className={"app-content"}>
                     <h1>BGO</h1>
-                    <Card title={"Card example"}
-                          description={"This is an example description"}
+                    <Card title={"D10"}
+                          description={"Roll a 10 sided die for movement"}
                           imagePath={"/images/tavern.png"}
+                          cardType={CARD_TYPE.ROLL}
+                          cardQuality={CARD_QUALITY.COMMON}
+                    />
+                    <Card title={"D20"}
+                          description={"Roll a 10 sided die for movement"}
+                          imagePath={"/images/tavern.png"}
+                          cardType={CARD_TYPE.SPELL}
+                          cardQuality={CARD_QUALITY.EPIC}
                     />
                 </div>
             </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,47 @@
 import React from "react";
 import "./Card.css";
 
-function Card({title, description, imagePath}) {
+export const CARD_TYPE = {
+    ITEM: "item",
+    SPELL: "spell",
+    CLASS: "class",
+    ROLL: "roll",
+};
+
+export const CARD_QUALITY = {
+    COMMON: {
+        name: "common",
+        color: "#f0f0f0",
+    },
+    UNCOMMON: {
+        name: "uncommon",
+        color: "#00ff00",
+    },
+    RARE: {
+        name: "rare",
+        color: "#0000ff",
+    },
+    EPIC: {
+        name: "epic",
+        color: "#ad05ad",
+    },
+    LEGENDARY: {
+        name: "legendary",
+        color: "#ffc907",
+    },
+}
+
+function Card({title, description, imagePath, cardType, cardQuality}) {
     return (
         <div className="card">
-            <h2 className={"title"}>{title}</h2>
+            <div className={"card-header"}>
+                <img src={`/images/card-icons/${cardType}-icon.png`} alt={""} width={30} style={{
+                    backgroundColor: cardQuality.color,
+                }}/>
+                <h2>
+                    {title}
+                </h2>
+            </div>
             <img src={imagePath} alt={""}/>
             <p>{description}</p>
         </div>
