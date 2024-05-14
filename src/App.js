1ada38d053eab24f88abe7bdcaf4dc3b89b8a3af
Corey Renton <Corey_Renton@outlook.com>
2024-05-14 11:36:14 +0100

    Aesthetic Update
    Testing how we could use PNG Blanks and Seperate Icons to make Cards on the fly.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,72 @@
-import './App.css';
+import "./App.css";
 import PlayerCards from "./components/PlayerCards";
 import EventPanel from "./components/EventPanel";
-import {useState} from "react";
-import {GameEventTypes} from "./classes/GameEventTypes";
-import {GameEvent} from "./classes/GameEvent";
+import { useState } from "react";
+import { GameEventTypes } from "./classes/GameEventTypes";
+import { GameEvent } from "./classes/GameEvent";
 import GameStateUpdater from "./components/utility/GameStateUpdater";
-import {TEST_PACK} from "./components/utility/DevUtils";
+import { TEST_PACK } from "./components/utility/DevUtils";
 
 function App() {
-    const [eventQueue, setEventQueue] = useState([GameEvent.TEST_DRAFT, GameEvent.TEST_DRAW]);
-    const [playerId, setPlayerId] = useState(0);
-    const [gameState, setGameState] = useState({
-        playerDeck: [],
-        playerHand: [],
-    });
+  const [eventQueue, setEventQueue] = useState([
+    GameEvent.TEST_DRAFT,
+    GameEvent.TEST_DRAW,
+  ]);
+  const [playerId, setPlayerId] = useState(0);
+  const [gameState, setGameState] = useState({
+    playerDeck: [],
+    playerHand: [],
+  });
 
-    const endCurrentEvent = () => {
-        setEventQueue(eventQueue.slice(1));
-    }
+  const endCurrentEvent = () => {
+    setEventQueue(eventQueue.slice(1));
+  };
 
-    const updateGameStateProperty = (key, value) => {
-        setGameState({...gameState, [key]: value});
-    }
+  const updateGameStateProperty = (key, value) => {
+    setGameState({ ...gameState, [key]: value });
+  };
 
-    const handleAddEvent = (event) => {
-        setEventQueue([...eventQueue, event]);
-    }
+  const handleAddEvent = (event) => {
+    setEventQueue([...eventQueue, event]);
+  };
 
-    const removePlayerCard = (cardToDelete) => {
-        updateGameStateProperty("playerHand", GameStateUpdater.removePlayerCardFromHand(gameState, cardToDelete));
-    }
-
-    const moveToNextEvent = () => {
-        if (eventQueue.length > 0) {
-            setEventQueue(eventQueue.slice(1));
-        } else {
-            setEventQueue([]);
-        }
-    }
-
-    return (
-        <div className="app-container">
-            <div className={"background-container"}>
-                <div className={"app-content"}>
-                    <EventPanel currentEvent={eventQueue[0]}
-                                removePlayerCard={removePlayerCard}
-                                moveToNextEvent={moveToNextEvent}
-                                gameState={gameState} setGameState={setGameState}
-                                endCurrentEvent={endCurrentEvent}
-                    />
-                    <PlayerCards
-                        playerHand={[...gameState.playerHand]}
-                        playerDeck={[...gameState.playerDeck]}
-                        addEvent={handleAddEvent}
-                        playerId={playerId}
-                    />
-                </div>
-            </div>
-        </div>
+  const removePlayerCard = (cardToDelete) => {
+    updateGameStateProperty(
+      "playerHand",
+      GameStateUpdater.removePlayerCardFromHand(gameState, cardToDelete)
     );
+  };
+
+  const moveToNextEvent = () => {
+    if (eventQueue.length > 0) {
+      setEventQueue(eventQueue.slice(1));
+    } else {
+      setEventQueue([]);
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <div className={"background-container"}>
+        <div className={"app-content"}>
+          <EventPanel
+            currentEvent={eventQueue[0]}
+            removePlayerCard={removePlayerCard}
+            moveToNextEvent={moveToNextEvent}
+            gameState={gameState}
+            setGameState={setGameState}
+            endCurrentEvent={endCurrentEvent}
+          />
+          <PlayerCards
+            playerHand={[...gameState.playerHand]}
+            playerDeck={[...gameState.playerDeck]}
+            addEvent={handleAddEvent}
+            playerId={playerId}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
